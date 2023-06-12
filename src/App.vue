f3e8758690bc7d21a5f449accbe2c1a0cc915dd3
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-06-11 21:54:05 -0400

    input ratio

--- src/App.vue
+++ src/App.vue
@@ -9,33 +9,29 @@
   {id: 3, label: "20 cups"}
   ])
 
-  const itemsObjects = ref({
-  'item-1': {id: 1, label: "10 party hats"},
-  'item-2': {id: 2, label: "2 board games"},
-  'item-3': {id: 3, label: "20 cups"}
-  })
+  const newItem = ref("")
+  const newItem2 = ref("")
+  const newItemPriority = ref("low")
+
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <input v-model.trim="newItem2" type="text" placeholder="Add an Item">
+  Priority: 
+  <label>
+    <input type="radio" v-model="newItemPriority" value="low">
+    Low
+  </label>
+  <label>
+    <input type="radio" v-model="newItemPriority" value="high">
+    High 
+  </label>
+  <br />
+  {{newItemPriority}}
   <ul>
-    <li v-for="item in items" :key="item.id">
-      {{item}}
-    </li>
-    <li v-for="item in items" :key="item.id">
-      {{item.label}}
-    </li>
-    <li v-for="{id, label} in items" :key="id">
-      {{id}} es a {{ label }}
-    </li>
     <li v-for="({id, label}, index) in items" :key="id">
-      {{id}} es {{ label }} : index = {{ index }}
-    </li>
-  </ul>
-  Print the object
-  <ul>
-    <li v-for="({id, label}, index) in itemsObjects" :key="id">
-      {{id}} es {{ label }} : index = {{ index }}
+      {{label}}
     </li>
   </ul>
 </template>
